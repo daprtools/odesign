29ad09e64f1b948e479b68f197fa5e7368c02aad
RKVS Raman <rkvsraman@gmail.com>
2025-02-03 09:17:32 +0530

    All icons set

--- io.dapr.daprdesigner.design/src/io/dapr/daprdesigner/design/Services.java
+++ io.dapr.daprdesigner.design/src/io/dapr/daprdesigner/design/Services.java
@@ -6,6 +6,8 @@ import org.eclipse.emf.ecore.EObject;
  * The services class used by VSM.
  */
 public class Services {
+	
+	int unnamedCounter = 0;
     
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
@@ -13,5 +15,10 @@ public class Services {
     public EObject myService(EObject self, String arg) {
        // TODO Auto-generated code
       return self;
+    }
+    
+    public String getUnnamed(EObject self) {
+    	unnamedCounter++;
+    	return "Unnamed_"+unnamedCounter;
     }
 }
